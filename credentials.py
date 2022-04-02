9fdcd8411a50c34c38cbd776bbbc5dddfd5ddaae
Vishal Marvel <vishal.pvn.edu@gmail.com>
2022-04-02 21:51:20 +0530

    added password streght

--- credentials.py
+++ credentials.py
@@ -1,17 +1,8 @@
 import os
-# sql = 'mysql://python:python_sql@localhost/users'
 sql = 'sqlite:///users.db'
-# sql = 'postgresql://agnzijncvhzwlp:ce33999773a480429519f3b6fc8eefae6791381e88a09853314f1649d3952780@ec2-52-73-155-171.compute-1.amazonaws.com:5432/dfimbv1oiootbf'
-# import os
 
 if sql.startswith("postgres://"):
     sql = sql.replace("postgres://", "postgresql://", 1)
-# sql = os.getenv("DATABASE_URL")  # or other relevant config var
-# if sql.startswith("postgres://"):
-#     sql = sql.replace("postgres://", "postgresql://", 1)
-    
-# sql = 'postgresql://postgres:heuwvybzcpaylc:436dabaec96ed3f7688aac3b516195df24abf2436fb73923c95a8f21bbe6e774@ec2-34-205-209-14.compute-1.amazonaws.com:5432/d2nsrdqrqkcik7'
-# sql = 'mysql://uboczaqyyznehlwt:gkDtUL1SFF5wnU9Du0MW@bmrujrdixbkkvxb3jjzx-mysql.services.clever-cloud.com/bmrujrdixbkkvxb3jjzx'
 
 key = "my super secret key that no one is supposed to know"
 
